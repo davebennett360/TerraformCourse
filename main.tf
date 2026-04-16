1e162a4760be3dacd5bef98de3c19d8d98ec9d8c
davebennett360 <dave.bennett360@gmail.com>
2026-04-16 11:46:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ terraform {
 }
 
 resource "google_compute_network" "example_vpc" {
-  name                    = "example-vpc"
+  name                    = "dave-vpc"
   auto_create_subnetworks = true 
 }
